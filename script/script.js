ed092783161cd8af7fae96e31e52013097e91aff
NickiSamantha <nickisabels73@gmail.com>
2024-05-14 14:08:13 +0200

    alldone

---
+++ script/script.js
@@ -0,0 +1,28 @@
+//Question 1 
+
+let firstName = 'Nicki'
+let lastName = 'Abels'
+
+//Question 2 
+
+console.log(firstName.concat(' ', lastName))
+
+//Question 3
+
+let numberVar = 5
+
+//Question 4
+
+console.log(numberVar * 5)
+
+//The ouput was 25. 
+
+//Question 5
+
+const name = 'Samantha'
+
+let name = 'Abels'
+
+//received the following error : 
+//VM418:1 Uncaught SyntaxError: Invalid or unexpected token
+
